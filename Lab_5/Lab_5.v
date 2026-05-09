a8d0940f6ae9146144c23270ade213f29d3a3062
Artem Opanasenko <opanasenko.artem@lll.kpi.ua>
2026-05-09 14:19:41 +0300

    correction

--- Lab_5/Lab_5.v
+++ Lab_5/Lab_5.v
@@ -1,14 +1,7 @@
 module lab5 (
     input  wire [3:0] D,
     input  wire [2:0] A,
-    output wire [3:0] Q0,
-    output wire [3:0] Q1,
-    output wire [3:0] Q2,
-    output wire [3:0] Q3,
-    output wire [3:0] Q4,
-    output wire [3:0] Q5,
-    output wire [3:0] Q6,
-    output wire [3:0] Q7
+    output wire [3:0] Q0, Q1, Q2, Q3, Q4, Q5, Q6, Q7
 );
 
     assign Q0 = {4{~A[2] & ~A[1] & ~A[0]}} & D; 
